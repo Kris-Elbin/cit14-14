e463f194b622da8db3c8685f9fcb515bf0de91b5
CIT142F-14@M307.UC-BCF.EDU.PH <CIT142F-14@M307.UC-BCF.EDU.PH>
2022-09-14 17:18:22 +0800

    w

--- push.bat
+++ push.bat
@@ -1,5 +1,5 @@
 @echo off
-git remote set-url origin https://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@github.com/Kris-Elbin/cit14-14.git
+git remote set -url origin https://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@github.com/Kris-Elbin/cit14-14.git
 pause
 git add . 
 git commit -m "w"
